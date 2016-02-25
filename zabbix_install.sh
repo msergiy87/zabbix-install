abe8ee3e94ded50c8089100a448a0ad01b8b1143
Sergiy <sergiy_007@ukr.net>
2016-02-25 10:30:01 +0200

    Update zabbix_install.sh

--- zabbix_install.sh
+++ zabbix_install.sh
@@ -22,9 +22,11 @@ apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-agent -y > /dev/n
 # Creating initial database
 mysql -e "UPDATE mysql.user SET Password = PASSWORD('MYSQL_ROOT_PASS') WHERE User = 'root';FLUSH PRIVILEGES;"
 
-mysql --user=root --password=MYSQL_ROOT_PASS -e "create database zabbix character set utf8 collate utf8_bin;grant all privileges on zabbix.* to zabbix@localhost identified by 'bCAui5PUqsf4l30kpM3S';FLUSH PRIVILEGES;"
+mysql --user=root --password=MYSQL_ROOT_PASS -e "create database zabbix character set utf8 collate utf8_bin;\
+grant all privileges on zabbix.* to zabbix@localhost identified by 'MYSQL_ZABBIX_PASS';\
+FLUSH PRIVILEGES;"
 
-zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql --user=root --password=MYSQL_ZABBIX_PASS zabbix
+zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql --user=root --password=MYSQL_ROOT_PASS zabbix
 
 # Starting Zabbix server process
 sed -i 's/# DBHost=.*/DBHost=localhost/' /etc/zabbix/zabbix_server.conf
@@ -35,11 +37,20 @@ sed -i 's/# DBPassword=.*/DBPassword=MYSQL_ZABBIX_PASS/' /etc/zabbix/zabbix_serv
 service zabbix-server restart
 
 # Editing PHP configuration for Zabbix frontend
-sed -i 's/^max_execution_time.*/max_execution_time = 300/' /etc/php5/fpm/php.ini
-sed -i 's/^memory_limit.*/memory_limit = 128M/' /etc/php5/fpm/php.ini
-sed -i 's/^post_max_size.*/post_max_size = 16M/' /etc/php5/fpm/php.ini
-sed -i 's/^upload_max_filesize.*/upload_max_filesize = 2M/' /etc/php5/fpm/php.ini
-sed -i 's/^max_input_time.*/max_input_time = 300/' /etc/php5/fpm/php.ini
+
+#sed -i 's/^max_execution_time.*/max_execution_time = 300/' /etc/php5/fpm/php.ini
+#sed -i 's/^memory_limit.*/memory_limit = 128M/' /etc/php5/fpm/php.ini
+#sed -i 's/^post_max_size.*/post_max_size = 16M/' /etc/php5/fpm/php.ini
+#sed -i 's/^upload_max_filesize.*/upload_max_filesize = 2M/' /etc/php5/fpm/php.ini
+#sed -i 's/^max_input_time.*/max_input_time = 300/' /etc/php5/fpm/php.ini
+#sed -i 's/^\;always_populate_raw_post_data =.*/always_populate_raw_post_data = -1/' /etc/php5/fpm/php.ini
+#sed -i "s/^\;date.timezone.*/date.timezone = \'Europe\/London\'/" /etc/php5/fpm/php.ini
+
+sed -i 's/^\(max_execution_time\).*/\1 = 300/' /etc/php5/fpm/php.ini
+sed -i 's/^\(memory_limit\).*/\1 = 128M/' /etc/php5/fpm/php.ini
+sed -i 's/^\(post_max_size\).*/\1 = 16M/' /etc/php5/fpm/php.ini
+sed -i 's/^\(upload_max_filesize\).*/\1 = 2M/' /etc/php5/fpm/php.ini
+sed -i 's/^\(max_input_time\).*/\1 = 300/' /etc/php5/fpm/php.ini
 sed -i 's/^\;always_populate_raw_post_data =.*/always_populate_raw_post_data = -1/' /etc/php5/fpm/php.ini
 sed -i "s/^\;date.timezone.*/date.timezone = \'Europe\/London\'/" /etc/php5/fpm/php.ini
 
